c435c8ceeeaf1351ca5c97cb159699627067d7ff
Pavel I. Kryukov <pavel.kryukov@phystech.edu>
2021-09-18 14:30:28 +0300

    Update copyright info

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Pavel I. Kryukov
+ * Copyright (c) 2019-2021 Pavel I. Kryukov
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
