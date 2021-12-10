a08b4efd4065ddb625b589e1b6a8778d80dfa76d
Pavel I. Kryukov <kryukov@frtk.ru>
2021-12-11 01:03:03 +0300

    Use Boost.Test

--- test/test.cpp
+++ test/test.cpp
@@ -21,22 +21,21 @@
  */
 
 #include "../aoaoaott.hpp"
-#include "catch.hpp"
 
-#include <cstring>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE CONTAINER
 
 #ifndef CONTAINER
 #error "CONTAINER macro must be defined"
 #endif
 
+#include <boost/test/included/unit_test.hpp>
+#include <cstring>
+
 #define PASTER(x,y) x ## y
 #define EVALUATOR(x,y) PASTER(x,y)
 #define VECTOR_CONTAINER EVALUATOR(CONTAINER, Vector)
 #define ARRAY_CONTAINER EVALUATOR(CONTAINER, Array)
-
-#define STRINGIZE(s) STRINGIZE_H(s)
-#define STRINGIZE_H(s) #s
-#define TEST_CONTAINER_CASE(x) TEST_CASE(STRINGIZE(CONTAINER) ": " x)
 
 using namespace aoaoaott;
 
@@ -46,7 +45,7 @@ constexpr const char* bold_cast(const R& ref)
     return reinterpret_cast<const char*>(&ref);
 }
 
-TEST_CONTAINER_CASE("principal test")
+BOOST_AUTO_TEST_CASE(principal_test)
 {
     struct Structure
     {
@@ -61,9 +60,9 @@ TEST_CONTAINER_CASE("principal test")
 
     // That is the only test dependent on container type
     if constexpr (std::is_same_v<ARRAY_CONTAINER<Structure, 20>, AoSArray<Structure, 20>>)
-        CHECK( distance == 10 * sizeof(Structure) );
+        BOOST_TEST( distance == 10 * sizeof(Structure) );
     else
-        CHECK( distance == 10 * sizeof(int) );
+        BOOST_TEST( distance == 10 * sizeof(int) );
 }
 
 struct A {
@@ -80,7 +79,7 @@ static_assert(std::is_same_v<decltype(array), AoSArray<A, 11>>
                 : distance == 10 * sizeof(int));
 */
 
-TEST_CONTAINER_CASE("initialize and r/w")
+BOOST_AUTO_TEST_CASE(initialize_and_rw)
 {
     VECTOR_CONTAINER<A> storage( 10);
     storage[3]->*(&A::key) = 10;
@@ -88,44 +87,44 @@ TEST_CONTAINER_CASE("initialize and r/w")
     storage[4]->*(&A::key) = 9;
     storage[4]->*(&A::val) = 6;
 
-    CHECK( storage[3]->*(&A::key) == 10 );
-    CHECK( storage[3]->*(&A::val) == 3 );
-    CHECK( storage[4]->*(&A::key) == 9 );
-    CHECK( storage[4]->*(&A::val) == 6 );
-}
+    BOOST_TEST( (storage[3]->*(&A::key)) == 10 );
+    BOOST_TEST( (storage[3]->*(&A::val)) == 3 );
+    BOOST_TEST( (storage[4]->*(&A::key)) == 9 );
+    BOOST_TEST( (storage[4]->*(&A::val)) == 6 );
+};
 
-TEST_CONTAINER_CASE("get() interface")
+BOOST_AUTO_TEST_CASE(get__interface)
 {
     VECTOR_CONTAINER<A> storage(10);
     storage[2].get<&A::val>() = 234;
-    CHECK( storage[2].get<&A::val>() == 234 );
-}
+    BOOST_TEST( storage[2].get<&A::val>() == 234 );
+};
 
-TEST_CONTAINER_CASE("assign structure")
+BOOST_AUTO_TEST_CASE(assign_structure)
 {
     VECTOR_CONTAINER<A> storage( 10);
     const A x{3, 7, 11};
     storage[2] = x; // copy assignment
     storage[3] = A{10, 3, 8}; // move assignment
 
-    CHECK( storage[2]->*(&A::val) == 3 );
-    CHECK( storage[2]->*(&A::key) == 7 );
-    CHECK( storage[2]->*(&A::dum) == 11 );
-    CHECK( storage[3]->*(&A::val) == 10 );
-    CHECK( storage[3]->*(&A::key) == 3 );
-    CHECK( storage[3]->*(&A::dum) == 8 );
+    BOOST_TEST( (storage[2]->*(&A::val)) == 3 );
+    BOOST_TEST( (storage[2]->*(&A::key)) == 7 );
+    BOOST_TEST( (storage[2]->*(&A::dum)) == 11 );
+    BOOST_TEST( (storage[3]->*(&A::val)) == 10 );
+    BOOST_TEST( (storage[3]->*(&A::key)) == 3 );
+    BOOST_TEST( (storage[3]->*(&A::dum)) == 8 );
 }
 
-TEST_CONTAINER_CASE("constant functions")
+BOOST_AUTO_TEST_CASE(constant_functions)
 {
     VECTOR_CONTAINER<A> storage( 10);
     storage[3]->*(&A::key) = 10;
     storage[3]->*(&A::val) = 3;
 
     const auto& const_ref = storage;
-    CHECK( const_ref[3]->*(&A::key) == 10 );
-    CHECK( const_ref[3]->*(&A::val) == 3 );
-    CHECK( const_ref[3].get<&A::val>() == 3 );
+    BOOST_TEST( (const_ref[3]->*(&A::key)) == 10 );
+    BOOST_TEST( (const_ref[3]->*(&A::val)) == 3 );
+    BOOST_TEST( (const_ref[3].get<&A::val>()) == 3 );
 }
 
 struct WithArray {
@@ -135,20 +134,20 @@ struct WithArray {
 
 static_assert(sizeof(WithArray) == sizeof(int) + 16);
 
-TEST_CONTAINER_CASE("structure with array")
+BOOST_AUTO_TEST_CASE(structure_with_array)
 {
     VECTOR_CONTAINER<WithArray> storage(10);
     std::memset(&(storage[3]->*(&WithArray::array)), 0x11, 16);
-    CHECK( (storage[3]->*(&WithArray::array))[8] == 0x11);
+    BOOST_TEST( ((storage[3]->*(&WithArray::array))[8]) == 0x11);
 }
 
-TEST_CONTAINER_CASE("assign array")
+BOOST_AUTO_TEST_CASE(assign_array)
 {
     VECTOR_CONTAINER<WithArray> storage( 10);
     WithArray hw{ 16, {"Hello World!!!!"}};
     storage[6] = hw;
 
-    CHECK( std::strcmp((storage[6]->*(&WithArray::array)).data(), "Hello World!!!!") == 0 );
+    BOOST_TEST( std::strcmp((storage[6]->*(&WithArray::array)).data(), "Hello World!!!!") == 0 );
 }
 
 struct DefaultInitializer
@@ -159,32 +158,32 @@ struct DefaultInitializer
 
 static_assert(!std::is_trivially_constructible_v<DefaultInitializer>);
 
-TEST_CONTAINER_CASE("default initialization")
+BOOST_AUTO_TEST_CASE(default_initialization)
 {
     VECTOR_CONTAINER<DefaultInitializer> storage( 10);
-    CHECK( storage[2]->*(&DefaultInitializer::x) == 9 );
-    CHECK( storage[3]->*(&DefaultInitializer::x) == 9 );
+    BOOST_TEST( (storage[2]->*(&DefaultInitializer::x)) == 9 );
+    BOOST_TEST( (storage[3]->*(&DefaultInitializer::x)) == 9 );
 }
 
-TEST_CONTAINER_CASE("initialization via copies")
+BOOST_AUTO_TEST_CASE(initialization_via_copies)
 {
     DefaultInitializer example{ 234, 123};
     VECTOR_CONTAINER<DefaultInitializer> storage( 10, example);
-    CHECK( storage[2]->*(&DefaultInitializer::x) == 234 );
-    CHECK( storage[2]->*(&DefaultInitializer::y) == 123 );
-    CHECK( storage[3]->*(&DefaultInitializer::x) == 234 );
+    BOOST_TEST( (storage[2]->*(&DefaultInitializer::x)) == 234 );
+    BOOST_TEST( (storage[2]->*(&DefaultInitializer::y)) == 123 );
+    BOOST_TEST( (storage[3]->*(&DefaultInitializer::x)) == 234 );
 }
 
-TEST_CONTAINER_CASE("resize by example")
+BOOST_AUTO_TEST_CASE(resize_by_example)
 {
     DefaultInitializer example{ 234, 123};
     VECTOR_CONTAINER<DefaultInitializer> storage(10);
     storage.resize(20, example);
-    CHECK( storage[5]->*(&DefaultInitializer::x) == 9 );
-    CHECK( storage[15]->*(&DefaultInitializer::x) == 234 );
+    BOOST_TEST( (storage[5]->*(&DefaultInitializer::x)) == 9 );
+    BOOST_TEST( (storage[15]->*(&DefaultInitializer::x)) == 234 );
 }
 
-TEST_CONTAINER_CASE("structure with constant member")
+BOOST_AUTO_TEST_CASE(structure_with_constant_member)
 {
     struct ConstantMember
     {
@@ -193,11 +192,11 @@ TEST_CONTAINER_CASE("structure with constant member")
     };
     VECTOR_CONTAINER<ConstantMember> storage;
     storage.resize(20);
-    CHECK( storage[5]->*(&ConstantMember::x) == 9 );
-    CHECK( storage[15]->*(&ConstantMember::x) == 9 );
+    BOOST_TEST( (storage[5]->*(&ConstantMember::x)) == 9 );
+    BOOST_TEST( (storage[15]->*(&ConstantMember::x)) == 9 );
 }
 
-TEST_CONTAINER_CASE("allow substructure")
+BOOST_AUTO_TEST_CASE(allow_substructure)
 {
     struct WithA {
         A a;
@@ -210,8 +209,8 @@ TEST_CONTAINER_CASE("allow substructure")
     const WithA y{x, 4, 8, 12};
     storage[5] = y;
 
-    CHECK( (storage[5]->*(&WithA::a)).dum == 11 );
-    CHECK( storage[5]->*(&WithA::dum) == 12 );
+    BOOST_TEST(((storage[5]->*(&WithA::a)).dum) == 11 );
+    BOOST_TEST((storage[5]->*(&WithA::dum)) == 12 );
 }
 
 struct HasMethod
@@ -230,44 +229,44 @@ struct HasMethod
     }
 };
 
-TEST_CONTAINER_CASE("aggregate and run method")
+BOOST_AUTO_TEST_CASE(aggregate_and_run_method)
 {
     VECTOR_CONTAINER<HasMethod> storage( 10);
     storage[4] = HasMethod{33, 44};
     auto val = storage[4].aggregate();
     val.drink_double_bourbon();
 
-    CHECK( val.alain == 44 );
-    CHECK( val.delon == 33 );
-    CHECK( storage[4]->*(&HasMethod::alain) == 33 );
-    CHECK( storage[4]->*(&HasMethod::delon) == 44 );
+    BOOST_TEST( val.alain == 44 );
+    BOOST_TEST( val.delon == 33 );
+    BOOST_TEST( (storage[4]->*(&HasMethod::alain)) == 33 );
+    BOOST_TEST( (storage[4]->*(&HasMethod::delon)) == 44 );
 }
 
-TEST_CONTAINER_CASE("implicit aggregate")
+BOOST_AUTO_TEST_CASE(implicit_aggregate)
 {
     VECTOR_CONTAINER<HasMethod> storage( 10);
     storage[4] = HasMethod{33, 44};
     HasMethod x(storage[4]);
     x.drink_double_bourbon();
 
-    CHECK( x.alain == 44 );
-    CHECK( x.delon == 33 );
+    BOOST_TEST( x.alain == 44 );
+    BOOST_TEST( x.delon == 33 );
 }
 
-TEST_CONTAINER_CASE("const iterator")
+BOOST_AUTO_TEST_CASE(const_iterator)
 {
     VECTOR_CONTAINER<A> storage(10, { 11, 12, 13});
     size_t i = 0;
     for (const auto& entry : storage) {
-        CHECK( entry->*(&A::val) == 11);
-        CHECK( entry->*(&A::key) == 12);
-        CHECK( entry->*(&A::dum) == 13);
+        BOOST_TEST( (entry->*(&A::val)) == 11);
+        BOOST_TEST( (entry->*(&A::key)) == 12);
+        BOOST_TEST( (entry->*(&A::dum)) == 13);
         ++i;
     }
-    CHECK(i == 10);
+    BOOST_TEST(i == 10);
 }
 
-TEST_CONTAINER_CASE("mutable iterator")
+BOOST_AUTO_TEST_CASE(mutable_iterator)
 {
     VECTOR_CONTAINER<A> storage(10);
     for (auto& entry : storage) {
@@ -276,44 +275,44 @@ TEST_CONTAINER_CASE("mutable iterator")
         entry->*(&A::dum) = 23;
     }
     for (const auto& entry : storage) {
-        CHECK( entry->*(&A::val) == 21);
-        CHECK( entry->*(&A::key) == 22);
-        CHECK( entry->*(&A::dum) == 23);
+        BOOST_TEST( (entry->*(&A::val)) == 21);
+        BOOST_TEST( (entry->*(&A::key)) == 22);
+        BOOST_TEST( (entry->*(&A::dum)) == 23);
     }
 }
 
-TEST_CONTAINER_CASE("iterator arrow operator")
+BOOST_AUTO_TEST_CASE(iterator_arrow_operator)
 {
     VECTOR_CONTAINER<A> storage(10, { 11, 12, 13});
-    CHECK( storage.cbegin()->get<&A::val>() == 11 );
-    CHECK( storage.begin()->get<&A::key>()  == 12 );
+    BOOST_TEST( storage.cbegin()->get<&A::val>() == 11 );
+    BOOST_TEST( storage.begin()->get<&A::key>()  == 12 );
 }
 
-TEST_CONTAINER_CASE("bidirectional iterator")
+BOOST_AUTO_TEST_CASE(bidirectional_iterator)
 {
     VECTOR_CONTAINER<A> storage(10, { 11, 12, 13});
     size_t i = 0;
     auto it = storage.cend();
     do {
         --it;
-        CHECK( *it->*(&A::val) == 11);
-        CHECK( *it->*(&A::key) == 12);
-        CHECK( *it->*(&A::dum) == 13);
+        BOOST_TEST( (*it->*(&A::val)) == 11);
+        BOOST_TEST( (*it->*(&A::key)) == 12);
+        BOOST_TEST( (*it->*(&A::dum)) == 13);
         ++i;
     } while (it != storage.cbegin());
-    CHECK(i == 10);
+    BOOST_TEST(i == 10);
 }
 
-TEST_CONTAINER_CASE("random access iterator")
+BOOST_AUTO_TEST_CASE(random_access_iterator)
 {
     VECTOR_CONTAINER<A> storage(90);
-    CHECK( storage.end() - storage.begin() == 90 );
-    CHECK( storage.begin() <= storage.end() );
+    BOOST_TEST((storage.end() - storage.begin()) == 90 );
+    BOOST_TEST((storage.begin() <= storage.end()));
     auto it = std::next(storage.begin(), 60);
-    CHECK( storage.end() - it == 30 );
+    BOOST_TEST((storage.end() - it) == 30 );
 }
 
-TEST_CONTAINER_CASE("reverse iterator")
+BOOST_AUTO_TEST_CASE(reverse_iterator)
 {
     VECTOR_CONTAINER<A> storage(10);
     int value = 20;
@@ -324,29 +323,29 @@ TEST_CONTAINER_CASE("reverse iterator")
     }
     size_t i = 0;
     for (auto it = storage.crbegin(); it != storage.crend(); ++it) {
-        CHECK( it->get<&A::val>() == value - 3 * i - 2 );
-        CHECK( it->get<&A::key>() == value - 3 * i - 1 );
-        CHECK( it->get<&A::dum>() == value - 3 * i );
+        BOOST_TEST( it->get<&A::val>() == value - 3 * i - 2 );
+        BOOST_TEST( it->get<&A::key>() == value - 3 * i - 1 );
+        BOOST_TEST( it->get<&A::dum>() == value - 3 * i );
         ++i;
     }
 }
 
-TEST_CONTAINER_CASE("const method")
+BOOST_AUTO_TEST_CASE(const_method)
 {
     const VECTOR_CONTAINER<HasMethod> storage( 10, HasMethod{33, 44});
-    CHECK( storage[3].method<&HasMethod::drink_cologne>(1) == 80);
+    BOOST_TEST( storage[3].method<&HasMethod::drink_cologne>(1) == 80);
 }
 
-TEST_CONTAINER_CASE("non-const method")
+BOOST_AUTO_TEST_CASE(non_const_method)
 {
     VECTOR_CONTAINER<HasMethod> storage( 10, HasMethod{33, 44});
     storage[3].method<&HasMethod::drink_double_bourbon>();
 
-    CHECK( storage[3]->*(&HasMethod::alain) == 44 );
-    CHECK( storage[3]->*(&HasMethod::delon) == 33 );
+    BOOST_TEST( (storage[3]->*(&HasMethod::alain)) == 44 );
+    BOOST_TEST( (storage[3]->*(&HasMethod::delon)) == 33 );
 }
 
-TEST_CONTAINER_CASE("const method and mutable field")
+BOOST_AUTO_TEST_CASE(const_method_and_mutable_field)
 {
     struct HasMutable {
         mutable int x;
@@ -355,25 +354,25 @@ TEST_CONTAINER_CASE("const method and mutable field")
 
     const VECTOR_CONTAINER<HasMutable> storage( 10, HasMutable{109});
     storage[3].method<&HasMutable::update_x>();
-    CHECK( storage[3]->*(&HasMutable::x) == 110 );
+    BOOST_TEST( (storage[3]->*(&HasMutable::x)) == 110 );
 }
 
-TEST_CONTAINER_CASE("arrow-star method")
+BOOST_AUTO_TEST_CASE(arrow_star_method)
 {
     const VECTOR_CONTAINER<HasMethod> storage( 10, HasMethod{33, 44});
-    CHECK( (storage[3]->*(&HasMethod::drink_cologne))(1) == 80);
+    BOOST_TEST( (storage[3]->*(&HasMethod::drink_cologne))(1) == 80);
 }
 
-TEST_CONTAINER_CASE("arrow-star non-const method")
+BOOST_AUTO_TEST_CASE(arrow_star_non_const_method)
 {
     VECTOR_CONTAINER<HasMethod> storage( 10, HasMethod{33, 44});
     (storage[3]->*(&HasMethod::drink_double_bourbon))();
 
-    CHECK( storage[3]->*(&HasMethod::alain) == 44 );
-    CHECK( storage[3]->*(&HasMethod::delon) == 33 );
+    BOOST_TEST( (storage[3]->*(&HasMethod::alain)) == 44 );
+    BOOST_TEST( (storage[3]->*(&HasMethod::delon)) == 33 );
 }
 
-TEST_CONTAINER_CASE("initialize array and r/w")
+BOOST_AUTO_TEST_CASE(initialize_array_and_r_w)
 {
     ARRAY_CONTAINER<A, 10> storage;
     storage[3]->*(&A::key) = 10;
@@ -381,72 +380,72 @@ TEST_CONTAINER_CASE("initialize array and r/w")
     storage[4]->*(&A::key) = 9;
     storage[4]->*(&A::val) = 6;
 
-    CHECK( storage[3]->*(&A::key) == 10 );
-    CHECK( storage[3]->*(&A::val) == 3 );
-    CHECK( storage[4]->*(&A::key) == 9 );
-    CHECK( storage[4]->*(&A::val) == 6 );
+    BOOST_TEST( (storage[3]->*(&A::key)) == 10 );
+    BOOST_TEST( (storage[3]->*(&A::val)) == 3 );
+    BOOST_TEST( (storage[4]->*(&A::key)) == 9 );
+    BOOST_TEST( (storage[4]->*(&A::val)) == 6 );
 }
 
-TEST_CONTAINER_CASE("array get() interface")
+BOOST_AUTO_TEST_CASE(array_get__interface)
 {
     ARRAY_CONTAINER<A, 10> storage;
     storage[2].get<&A::val>() = 234;
-    CHECK( storage[2].get<&A::val>() == 234 );
+    BOOST_TEST( storage[2].get<&A::val>() == 234 );
 }
 
-TEST_CONTAINER_CASE("array assign structure")
+BOOST_AUTO_TEST_CASE(array_assign_structure)
 {
     ARRAY_CONTAINER<A, 10> storage;
     const A x{3, 7, 11};
     storage[2] = x; // copy assignment
     storage[3] = A{10, 3, 8}; // move assignment
 
-    CHECK( storage[2]->*(&A::val) == 3 );
-    CHECK( storage[2]->*(&A::key) == 7 );
-    CHECK( storage[2]->*(&A::dum) == 11 );
-    CHECK( storage[3]->*(&A::val) == 10 );
-    CHECK( storage[3]->*(&A::key) == 3 );
-    CHECK( storage[3]->*(&A::dum) == 8 );
+    BOOST_TEST( (storage[2]->*(&A::val)) == 3 );
+    BOOST_TEST( (storage[2]->*(&A::key)) == 7 );
+    BOOST_TEST( (storage[2]->*(&A::dum)) == 11 );
+    BOOST_TEST( (storage[3]->*(&A::val)) == 10 );
+    BOOST_TEST( (storage[3]->*(&A::key)) == 3 );
+    BOOST_TEST( (storage[3]->*(&A::dum)) == 8 );
 }
 
-TEST_CONTAINER_CASE("vector methods")
+BOOST_AUTO_TEST_CASE(vector_methods)
 {
     VECTOR_CONTAINER<A> storage;
     storage.push_back(A{3, 14, 15});
-    CHECK( storage[0]->*(&A::val) == 3 );
-    CHECK( !storage.empty() );
-    CHECK_THROWS_AS( storage.at(1000), std::out_of_range );
-    CHECK( storage.front()->*(&A::dum) == 15);
-    CHECK( storage.back()->*(&A::dum) == 15);
+    BOOST_TEST( (storage[0]->*(&A::val)) == 3 );
+    BOOST_TEST( !storage.empty() );
+    BOOST_CHECK_THROW( storage.at(1000), std::out_of_range );
+    BOOST_TEST( (storage.front()->*(&A::dum)) == 15);
+    BOOST_TEST( (storage.back()->*(&A::dum)) == 15);
 }
 
-TEST_CONTAINER_CASE("vector assign")
+BOOST_AUTO_TEST_CASE(vector_assign)
 {
     VECTOR_CONTAINER<A> storage(10, A{3, 14, 15});
     storage.assign(5, A{2, 7, 1828});
-    CHECK( storage[7]->*(&A::dum) == 15 );
-    CHECK( storage[2]->*(&A::dum) == 1828 );
+    BOOST_TEST( (storage[7]->*(&A::dum)) == 15 );
+    BOOST_TEST( (storage[2]->*(&A::dum)) == 1828 );
 }
 
-TEST_CONTAINER_CASE("vector capacity")
+BOOST_AUTO_TEST_CASE(vector_capacity)
 {
     VECTOR_CONTAINER<A> storage(10);
     storage.reserve(200);
-    CHECK( storage.capacity() >= 200 );
-    CHECK( storage.size() == 10 );
+    BOOST_TEST( storage.capacity() >= 200 );
+    BOOST_TEST( storage.size() == 10 );
 
     storage.shrink_to_fit();
-    CHECK( storage.capacity() >= 10 );
+    BOOST_TEST( storage.capacity() >= 10 );
 }
 
-TEST_CONTAINER_CASE("array fill")
+BOOST_AUTO_TEST_CASE(array_fill)
 {
     ARRAY_CONTAINER<A, 100> storage;
     storage.fill(A{2, 7, 1828});
-    CHECK( storage[93]->*(&A::dum) == 1828 );
+    BOOST_TEST( (storage[93]->*(&A::dum)) == 1828 );
 }
 
-TEST_CONTAINER_CASE("move-semantics")
+BOOST_AUTO_TEST_CASE(move_semantics)
 {
     struct Point
     {
@@ -459,13 +458,13 @@ TEST_CONTAINER_CASE("move-semantics")
     storage[3] = Point{2, 4, std::make_unique<int>(10)};
     storage[4] = Point{2, 4, std::make_unique<int>(7)};
 
-    CHECK( storage[3]->*(&Point::x) == 2 );
-    CHECK( storage[3]->*(&Point::y) == 4 );
-    CHECK( *(storage[3]->*(&Point::z)) == 10 );
+    BOOST_TEST( (storage[3]->*(&Point::x)) == 2 );
+    BOOST_TEST( (storage[3]->*(&Point::y)) == 4 );
+    BOOST_TEST( *(storage[3]->*(&Point::z)) == 10 );
 
     Point point = storage[4].aggregate_move();
-    CHECK( *(point.z) == 7 );
+    BOOST_TEST( *(point.z) == 7 );
 
     Point point2 = std::move(storage[3]);
-    CHECK( *(point2.z) == 10 );
+    BOOST_TEST( *(point2.z) == 10 );
 }
